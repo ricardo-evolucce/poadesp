82fe0ad927dc99bf3917bdb0612ae42f3a89ba9a
Ricardo Müller <ricardo.evolucce@gmail.com>
2021-01-21 15:21:50 -0300

    script analytics

--- includes/footer.php
+++ includes/footer.php
@@ -22,3 +22,14 @@
 			</div>
 		</div>
 	</footer>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-187804976-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-187804976-1');
+</script>
